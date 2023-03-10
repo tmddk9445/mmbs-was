60295005f2c284b60716cca259348f46aea7cd2b
tmddk9445 <tmddk9445@naver.com>
2023-03-10 14:00:25 +0900

    20230310 FRI mmbs-was Code Refactoring OrderCheckEntity - view

---
+++ src/main/java/com/mong/mmbs/entity/OrderCheckEntity.java
@@ -0,0 +1,39 @@
+package com.mong.mmbs.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Immutable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Immutable
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orderCheck")
+@Entity(name = "orderCheck")
+public class OrderCheckEntity {
+
+  private String orderNumber;
+  // 개당 금액
+  private int productPrice;
+  // 제품 이름
+  private String productTitle;
+  // 제품 이미지
+  private String productImageUrl;
+  // 갯수
+  private int productCount;
+
+  // 주문 날짜
+  private String orderDatetime;
+  // 주문 상태
+  private int orderStatus;
+  // 최종 결제 금액
+  private int orderTotalPrice;
+  // 배송 회사
+  private String orderShipCompany;
+
+}
